cb849ffecd21c15b890e9458e17c10d1273342cd
duc.pa <duc.pa@digidinos.com>
2020-10-26 14:17:27 +0700

    🍗 Menu seeder

--- database/seeds/MenuSeeder.php
+++ database/seeds/MenuSeeder.php
@@ -24,19 +24,19 @@ class MenuSeeder extends Seeder
 
         // user groups
         $userMenu = Menu::updateOrCreate(
-            ['title' => 'user'],
+            ['title' => 'module.user'],
             ['link' => '/user', 'position' => 1, 'icon' => 'user-friends',  'parent_id' => 0]
         );
 
         // role groups
         $roleMenu = Menu::updateOrCreate(
-            ['title' => 'role'],
+            ['title' => 'module.role'],
             ['link' => '/role', 'position' => 2, 'icon' => 'balance-scale',  'parent_id' => 0]
         );
 
         // menu groups
         $menuMenu = Menu::updateOrCreate(
-            ['title' => 'menu'],
+            ['title' => 'module.menu'],
             ['link' => '/menu', 'position' => 3,  'icon' => 'list',  'parent_id' => 0]
         );
     }
